42691c347b461e1ee24560190535ac007f99f7bb
Hugo López Fernández <hlfernandez@uvigo.es>
2015-04-11 18:27:42 +0200

    Add ExtendedJXTable component
    Add ExtendedJXTable component that extends JXTable from swingx library.

---
+++ src/main/java/es/uvigo/ei/hlfernandez/demo/ExtendedJXTableDemo.java
@@ -0,0 +1,47 @@
+package es.uvigo.ei.hlfernandez.demo;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+
+import es.uvigo.ei.sing.hlfernandez.table.ExtendedJXTable;
+
+/**
+ * An example showing the use of {@link ExtendedJXTable}.
+ * 
+ * @author hlfernandez
+ *
+ */
+public class ExtendedJXTableDemo {
+	private static final String columnNames[] = 
+		{ "Column 1", "Column 2", "Column 3" };
+
+	private static final String dataValues[][] =
+			{
+				{ "12", "234", "67" },
+				{ "-123", "43", "853" },
+				{ "93", "89.2", "109" },
+				{ "279", "9033", "3092" }
+			};
+	
+	public static void main(String[] args) {
+		java.util.Locale.setDefault(java.util.Locale.ENGLISH);
+		
+		ExtendedJXTable table = new ExtendedJXTable(dataValues, columnNames);
+		table.setColumnControlVisible(true);
+		table.addAction(new AbstractAction("Demo Action") {
+			
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Demo action");
+			}
+		});
+		table.setColumVisibilityActionsEnabled(false);
+		
+		DemoUtils.showComponent(new JScrollPane(table), "ExtendedJXTable demo dialog");
+	}
+}
---
+++ src/main/java/es/uvigo/ei/sing/hlfernandez/table/ExtendedJXTable.java
@@ -0,0 +1,146 @@
+package es.uvigo.ei.sing.hlfernandez.table;
+
+import java.util.Vector;
+
+import javax.swing.Action;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+
+import org.jdesktop.swingx.JXTable;
+import org.jdesktop.swingx.table.ColumnControlButton;
+
+/**
+ * An extension of {@link JXTable} that allows to hide/show the column
+ * visibility actions and also facilitates adding own actions to the
+ * {@code ColumnControlButton}.
+ * 
+ * @author hlfernandez
+ *
+ */
+public class ExtendedJXTable extends JXTable {
+
+	private static final long serialVersionUID = 1L;
+	private CustomColumnControlButton columnControlButton;
+	private boolean showVisibilityActions = true; 
+
+	/** 
+	 * Instantiates an ExtendedJXTable with a default table model, no data. 
+	 */
+    public ExtendedJXTable() {
+        super();
+        init();
+    }
+
+    /**
+     * Instantiates an ExtendedJXTable with a specific table model.
+     * 
+     * @param dm The model to use.
+     */
+    public ExtendedJXTable(TableModel dm) {
+        super(dm);
+        init();
+    }
+
+    /**
+     * Instantiates an ExtendedJXTable with a specific table model.
+     * 
+     * @param dm The model to use.
+     */
+    public ExtendedJXTable(TableModel dm, TableColumnModel cm) {
+        super(dm, cm);
+        init();
+    }
+
+    /**
+     * Instantiates an ExtendedJXTable with a specific table model, column model, and
+     * selection model.
+     * 
+     * @param dm The table model to use.
+     * @param cm The column model to use.
+     * @param sm The list selection model to use.
+     */
+    public ExtendedJXTable(TableModel dm, TableColumnModel cm, ListSelectionModel sm) {
+        super(dm, cm, sm);
+        init();
+    }
+
+    /**
+     * Instantiates an ExtendedJXTable for a given number of columns and rows.
+     * 
+     * @param numRows Count of rows to accommodate.
+     * @param numColumns Count of columns to accommodate.
+     */
+    public ExtendedJXTable(int numRows, int numColumns) {
+        super(numRows, numColumns);
+        init();
+    }
+
+    /**
+     * Instantiates an ExtendedJXTable with data in a vector or rows and column names.
+     * 
+     * @param rowData Row data, as a Vector of Objects.
+     * @param columnNames Column names, as a Vector of Strings.
+     */
+    public ExtendedJXTable(Vector<?> rowData, Vector<?> columnNames) {
+        super(rowData, columnNames);
+        init();
+    }
+
+    /**
+     * Instantiates an ExtendedJXTable with data in a array or rows and column names.
+     * 
+     * @param rowData Row data, as a two-dimensional Array of Objects (by row,
+     *        for column).
+     * @param columnNames Column names, as a Array of Strings.
+     */
+    public ExtendedJXTable(Object[][] rowData, Object[] columnNames) {
+        super(rowData, columnNames);
+        init();
+    }
+
+    private void init(){
+    	columnControlButton = new CustomColumnControlButton(this);
+    	setColumnControl(columnControlButton);
+    }
+    
+    /**
+	 * Adds {@code Action a} so that it will appear in the
+	 * {@code ColumnControlButton} of the table.
+	 * 
+	 * @param a The action to add.
+	 */
+	public void addAction(Action a) {
+		this.getActionMap().put(ColumnControlButton.COLUMN_CONTROL_MARKER+a.getValue(Action.NAME), a);
+		this.columnControlButton.update();
+	}
+	
+	
+	/**
+	 * Sets the visibility of the column visibility actions at the {@code ColumnControlButton}.
+	 * 
+	 * @param enabled If true, the column visibility actions are displayed. 
+	 */
+	public void setColumVisibilityActionsEnabled(boolean enabled) {
+		this.showVisibilityActions = enabled;
+		this.columnControlButton.update();
+	}
+
+	private class CustomColumnControlButton extends ColumnControlButton {
+		private static final long serialVersionUID = 1L;
+
+		public CustomColumnControlButton(JXTable table) {
+			super(table);
+		}
+
+		protected void createVisibilityActions() {
+			if(showVisibilityActions == true){
+				super.createVisibilityActions();
+			}
+		}
+	
+		public void update() {
+			super.populatePopup();
+		}
+	}
+}
