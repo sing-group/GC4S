af0f10c2ce518c26ecdebd324ca99a8c022a5fbe
Hugo López Fernández <hlfernandez@uvigo.es>
2016-08-31 11:16:42 +0100

    Add FilterableJXTable component

---
+++ src/main/java/es/uvigo/ei/sing/hlfernandez/demo/FilterableJXTableDemo.java
@@ -0,0 +1,89 @@
+package es.uvigo.ei.sing.hlfernandez.demo;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.AbstractAction;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import es.uvigo.ei.sing.hlfernandez.table.FilterableJXTable;
+
+/**
+ * An example showing the use of {@link FilterableJXTable}.
+ * 
+ * @author hlfernandez
+ *
+ */
+public class FilterableJXTableDemo {
+	private static final String columnNames[] = 
+		{"#", "Column 1", "Column 2", "Column 3" };
+
+	private static final String dataValues[][] =
+			{
+				{ "1", "12", "234", "67" },
+				{ "2", "-123", "43", "853" },
+				{ "3", "93", "89.3", "119" },
+				{ "4", "94", "89.4", "139" },
+				{ "5", "95", "89.5", "129" },
+				{ "6", "96", "89.6", "119" },
+				{ "7", "279", "9033", "3092" }
+			};
+	
+	public static void main(String[] args) {
+		java.util.Locale.setDefault(java.util.Locale.ENGLISH);
+		
+		FilterableJXTable table = new FilterableJXTable(dataValues, columnNames);
+		table.setColumnControlVisible(true);
+		table.setColumVisibilityActionsEnabled(false);
+		
+		DemoUtils.showComponent(createDemoPanel(table), "FilterableJXTable demo dialog");
+	}
+
+	private static JPanel createDemoPanel(FilterableJXTable table) {
+		JPanel panel = new JPanel(new BorderLayout());
+		
+		JPanel controlPanel = new JPanel(new BorderLayout());
+		JTextField filter = new JTextField();
+		JButton applyFilter = new JButton(new AbstractAction("Filter") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				List<String> rows = Arrays.asList(filter.getText().split(","));
+				List<Integer> visibleRows = 
+					rows.stream().map(Integer::new)
+					.map(i -> new Integer(i-1))
+					.collect(Collectors.toList());
+				table.setVisibleRows(visibleRows);
+			}
+		});
+		
+		JButton removeFilter = new JButton(new AbstractAction("Remove filter") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				table.setAllRowsVisible();
+			}
+		});
+		
+		JPanel buttonsPanel = new JPanel(new BorderLayout());
+		buttonsPanel.add(applyFilter, BorderLayout.WEST);
+		buttonsPanel.add(Box.createHorizontalStrut(5), BorderLayout.CENTER);
+		buttonsPanel.add(removeFilter, BorderLayout.EAST);
+		
+		controlPanel.add(filter, BorderLayout.CENTER);
+		controlPanel.add(buttonsPanel, BorderLayout.EAST);
+		
+		panel.add(controlPanel, BorderLayout.NORTH);
+		panel.add(new JScrollPane(table), BorderLayout.CENTER);
+		return panel;
+	}
+}
---
+++ src/main/java/es/uvigo/ei/sing/hlfernandez/table/FilterableJXTable.java
@@ -0,0 +1,75 @@
+package es.uvigo.ei.sing.hlfernandez.table;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.swing.RowFilter;
+
+import org.jdesktop.swingx.JXTable;
+
+/**
+ * An extension of {@link JXTable} that allows to establish a row filter.
+ * 
+ * @author hlfernandez
+ *
+ */
+public class FilterableJXTable extends ExtendedJXTable {
+	private static final long serialVersionUID = 1L;
+	private Optional<List<Integer>> visibleRows;
+
+    /**
+     * Instantiates an FilterableJXTable with data in a array or rows and column
+     *  names.
+     * 
+     * @param rowData Row data, as a two-dimensional Array of Objects (by row,
+     * 	for column).
+     * @param columnNames Column names, as a Array of Strings.
+     */
+	public FilterableJXTable(String[][] datavalues, String[] columnnames) {
+		super(datavalues, columnnames);
+		visibleRows = Optional.empty();
+	}
+
+	/**
+	 * Sets all rows visible (i.e. removes the current row filter).
+	 */
+	public void setAllRowsVisible() {
+		visibleRows = Optional.empty();
+		updateRowFilter();
+	}
+
+	/**
+	 * Sets visible only row indexes specified in {@code visibleRows}. 
+	 * 
+	 * @param visibleRows a list containing the row indexes that must remain
+	 * 	visible.
+	 */
+	public void setVisibleRows(List<Integer> visibleRows) {
+		this.visibleRows = Optional.of(visibleRows);
+		updateRowFilter();
+	}
+	
+	private void updateRowFilter() {
+		this.setRowFilter(getRowFilter());
+	}
+
+	public TestRowFilter<Object, Object> getRowFilter() {
+		return new TestRowFilter<>();
+	}
+	
+	public Optional<List<Integer>> getVisibleRows() {
+		return visibleRows;
+	}
+
+	class TestRowFilter<M, I> extends RowFilter<M, I> {
+		
+		@Override
+		public boolean include(RowFilter.Entry<? extends M, ? extends I> entry) {
+			if(getVisibleRows().isPresent()) {
+				return getVisibleRows().get().contains(entry.getIdentifier());
+			} else {
+				return true;
+			}
+		}
+	}
+}
