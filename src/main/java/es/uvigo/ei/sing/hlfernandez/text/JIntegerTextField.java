05bbee7467e87ed30dce0ac3557562ee90523b70
Hugo López Fernández <hlfernandez@uvigo.es>
2016-09-30 15:30:11 +0100

    Add JIntegerTextField component

---
+++ src/main/java/es/uvigo/ei/sing/hlfernandez/text/JIntegerTextField.java
@@ -0,0 +1,49 @@
+package es.uvigo.ei.sing.hlfernandez.text;
+
+import java.text.NumberFormat;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.text.NumberFormatter;
+
+import es.uvigo.ei.sing.hlfernandez.utilities.TextFieldSelectionFocusListener;
+
+/**
+ * This class extends {@code JFormattedTextField} and only accepts integers as
+ * input.
+ * 
+ * @author hlfernandez
+ *
+ */
+public class JIntegerTextField extends JFormattedTextField {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a new {@code JIntegerTextField} and initializes it with the
+	 * specified value.
+	 * 
+	 * @param value
+	 *            the initial value.
+	 */
+	public JIntegerTextField(Integer value) {
+	    this.setFormatter(configureFormatter());
+	    this.setValue(0);
+	    this.addFocusListener(new TextFieldSelectionFocusListener());
+	}
+
+	private AbstractFormatter configureFormatter() {
+		NumberFormat format = NumberFormat.getInstance();
+	    NumberFormatter formatter = new NumberFormatter(format);
+	    formatter.setValueClass(Integer.class);
+	    formatter.setMinimum(0);
+	    formatter.setMaximum(Integer.MAX_VALUE);
+	    formatter.setAllowsInvalid(false);
+	    formatter.setCommitsOnValidEdit(true);
+	    
+	    return formatter;
+	}
+	
+	@Override
+	public Integer getValue() {
+		return (Integer) super.getValue();
+	}
+}
