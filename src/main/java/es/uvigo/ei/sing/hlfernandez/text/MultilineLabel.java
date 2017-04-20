84d3ae9f62b8fbae358b977a586431409c38a13f
Hugo López Fernández <hlfernandez@uvigo.es>
2017-04-20 15:42:04 +0100

    Add MultilineLabel component

---
+++ src/main/java/es/uvigo/ei/sing/hlfernandez/text/MultilineLabel.java
@@ -0,0 +1,38 @@
+package es.uvigo.ei.sing.hlfernandez.text;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+
+/**
+ * An extension of {@code JTextArea} configured as multiline label.
+ * 
+ * @author hlfernandez
+ *
+ */
+public class MultilineLabel extends JTextArea {
+	private static final long serialVersionUID = 1L;
+	private String text;
+
+	/**
+	 * Creates a new {@code MultilineLabel} to show the specified {@code text}.
+	 * 
+	 * @param text the text of the label
+	 */
+	public MultilineLabel(String text) {
+		this.text = text;
+
+		this.init();
+	}
+
+	private void init() {
+		this.setAlignmentX(LEFT_ALIGNMENT);
+		this.setEditable(false);
+		this.setLineWrap(true);
+		this.setWrapStyleWord(true);
+		this.setOpaque(false);
+		this.setBorder(BorderFactory.createEmptyBorder());
+		this.setFont(UIManager.getFont("Label.font"));
+		this.setText(text);
+	}
+}
