73341ec1dbc9a4c4810d895e7a42d03741bf0033
Hugo López Fernández <hlfernandez@uvigo.es>
2017-04-26 09:50:05 +0100

    Allow setting custom JProgressDialog labels

--- src/main/java/es/uvigo/ei/sing/hlfernandez/dialog/JProgressDialog.java
+++ src/main/java/es/uvigo/ei/sing/hlfernandez/dialog/JProgressDialog.java
@@ -14,13 +14,15 @@ import javax.swing.JProgressBar;
 /**
  * A {@code JProgressDialog} facilitates the creation of progress dialogs based
  * on fixed task lists.
- * 
+ *
  * @author hlfernandez
  *
  */
 public class JProgressDialog extends JDialog {
-	
 	private static final long serialVersionUID = 1L;
+	private static final String DEFAULT_LABEL_CURRENT_TASK = "Current task:";
+	private static final String DEFAULT_LABEL_TOTAL_PROGRESS = "Total progress:";
+
 	private JFrame parent;
 	private List<String> tasks;
 	private JLabel taskLabel;
@@ -28,18 +30,57 @@ public class JProgressDialog extends JDialog {
 	private Iterator<String> tasksIterator;
 	private int increment;
 	private JPanel content;
-	
+	private String currentTaskLabel;
+	private String totalProgressLabel;
+
 	/**
-	 * Constructs a new {@code ProgressDialog} instance.
-	 * 
-	 * @param parent the parent frame to show the dialog.
-	 * @param title the dialog title.
-	 * @param tasks the list of tasks.
+	 * Constructs a new {@code ProgressDialog} instance with the specified
+	 * parameters.
+	 *
+	 * @param parent the parent frame to show the dialog
+	 * @param title the dialog title
+	 * @param tasks the list of tasks
 	 */
 	public JProgressDialog(JFrame parent, String title, List<String> tasks) {
+		this(parent, title, tasks, DEFAULT_LABEL_CURRENT_TASK,
+			DEFAULT_LABEL_TOTAL_PROGRESS);
+	}
+
+	/**
+	 * Constructs a new {@code ProgressDialog} instance with the specified
+	 * parameters.
+	 *
+	 * @param parent the parent frame to show the dialog
+	 * @param title the dialog title
+	 * @param tasks the list of tasks
+	 * @param tasks the list of tasks
+	 * @param currentTaskLabel the label of the current task
+	 */
+	public JProgressDialog(JFrame parent, String title, List<String> tasks,
+		String currentTaskLabel) {
+		this(parent, title, tasks, currentTaskLabel,
+			DEFAULT_LABEL_TOTAL_PROGRESS);
+	}
+
+	/**
+	 * Constructs a new {@code ProgressDialog} instance with the specified
+	 * parameters.
+	 *
+	 * @param parent the parent frame to show the dialog
+	 * @param title the dialog title
+	 * @param tasks the list of tasks
+	 * @param tasks the list of tasks
+	 * @param currentTaskLabel the label of the current task
+	 * @param totalProgressLabel the label of the total progress bar
+	 */
+	public JProgressDialog(JFrame parent, String title, List<String> tasks,
+		String currentTaskLabel, String totalProgressLabel) {
 		super(parent, title);
+
 		this.parent = parent;
 		this.tasks = tasks;
+		this.currentTaskLabel = currentTaskLabel;
+		this.totalProgressLabel = totalProgressLabel;
 		this.tasksIterator = this.tasks.iterator();
 		this.increment = 100 / tasks.size();
 		this.initComponent();
@@ -49,16 +90,16 @@ public class JProgressDialog extends JDialog {
 		this.content = new JPanel();
 		this.content.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		this.content.setLayout(new GridLayout(2, 2));
-		
+
 		progressBar = new JProgressBar();
 		progressBar.setStringPainted(true);
 		progressBar.setValue(0);
-		
+
 		taskLabel = new JLabel(tasksIterator.next());
 
-		this.content.add(new JLabel("Current task:"));
+		this.content.add(new JLabel(currentTaskLabel));
 		this.content.add(taskLabel);
-		this.content.add(new JLabel("Total progress:"));
+		this.content.add(new JLabel(totalProgressLabel));
 		this.content.add(progressBar);
 
 		this.add(this.content);
@@ -69,26 +110,25 @@ public class JProgressDialog extends JDialog {
 
 	/**
 	 * Sets the progress finished.
-	 * 
+	 *
 	 * @param message the message to show in the label.
 	 */
 	public void finished(String message) {
 		this.progressBar.setValue(100);
 		this.taskLabel.setText(message);
-		
 	}
 
 	/**
 	 * Sets next task in the label and increments the progress.
 	 */
 	public void nextTask() {
-		if(tasksIterator.hasNext()) {
-			this.progressBar.setValue(this.progressBar.getValue()+this.increment);
+		if (tasksIterator.hasNext()) {
+			this.progressBar
+				.setValue(this.progressBar.getValue() + this.increment);
 			this.taskLabel.setText(tasksIterator.next());
 		} else {
 			this.progressBar.setValue(100);
 		}
-		
 	}
 
 	protected void centerDialogOnScreen() {
